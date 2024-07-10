a2c25dae96c2633d52be7c3a4a9ad31226e9821e
keshav Garg <garg68860@gmail.com>
2024-07-10 14:19:53 +0530

    This is javascript array

---
+++ Array.js
@@ -0,0 +1,34 @@
+
+// let newarray= new Array(1,2,34,44)
+// console.log(newarray.includes(3));
+// console.log(newarray.indexOf(34));
+
+// let arr=newarray.join(" ")
+// console.log(arr);
+
+// let arr2=[1,2,3,4,5,6]
+// console.log(arr2.slice(2,5));
+// console.log(arr2.splice(2,2,"hi","hello","tata","byebye"));
+// console.log(arr2);
+
+// const arr1=[1,2,3,4]
+// const arr2=[3,46,6,34]
+
+// arr1.push(arr2)
+// console.log(arr1);
+// const arr3=arr1.concat(arr2)
+// console.log(arr3)
+
+// const arr3=[...arr1,...arr2]
+// console.log(arr3);
+
+
+const arr4=[1,[2,3,[56,[34,56,67,8,8]]]]
+const a="keshav"
+const b="Garg"
+const c="is the best"
+console.log(arr4.flat(Infinity));
+
+console.log(Array.isArray(arr3))
+console.log(Array.from("keshav_The_Rancho"))
+console.log(Array.of(a,b,c))
